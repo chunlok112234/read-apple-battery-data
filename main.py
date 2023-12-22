f57e30aae86e431590dae8a23531a10a9b95bd2f
chunlok112234 <144273133+chunlok112234@users.noreply.github.com>
2023-12-23 03:48:30 +0800

    plt

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import json
 from colorama import Fore
 from datetime import datetime as dt
+import matplotlib as mat
+import matplotlib.pyplot as plt 
 
 data_floder = "./analytics-data"
 filename_ls = os.listdir(data_floder)
@@ -9,6 +11,10 @@ date_fmt = "%Y-%m-%d"
 device_json: dict = json.loads(open("device.json").read())
 designed_capacity = None
 opt: dict|str|None = device_json
+date_list = []
+cycle_list = []
+health_list = []
+
 while 1:
     for index, ele in enumerate(opt.keys()):
         print(f"{Fore.BLUE}{index+1}.{Fore.RESET} {Fore.YELLOW}{ele}{Fore.RESET}")
@@ -42,6 +48,25 @@ for filename in filename_ls:
 {Fore.CYAN}Capacity:{Fore.RESET} {Fore.GREEN if health_percentage > 80 else Fore.YELLOW}{last_value_NominalChargeCapacity} mAh{Fore.RESET}
 {Fore.CYAN}Health:{Fore.RESET} {Fore.GREEN if health_percentage > 80 else Fore.YELLOW}{health_percentage:.2f}%{Fore.RESET}
 {'='*20}""")
+                date_list.append(datetime_obj)
+                cycle_list.append(last_value_CycleCount)
+                health_list.append(health_percentage)
                 break
         if content_obj.get("_marker") == "<end-of-file>":
             print(f"No battery data is found on {datetime_obj.strftime(date_fmt)}")
+
+# print(date_list,cycle_list, health_list )
+            
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+
+ax1.plot(date_list, cycle_list, 'g', marker='s')
+ax2.plot(date_list, health_list, 'b', marker='o')
+
+ax1.set_xlabel('date')
+ax1.set_ylabel('cycle', color='g')
+ax2.set_ylabel('health (%)', color='b')
+
+fig.gca().set_aspect(2)
+plt.show()
